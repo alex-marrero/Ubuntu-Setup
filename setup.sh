07228d975d3a16589cbf9c43feea3c4ef4b22ae3
Alexander Marrero <58573200+alex-marrero@users.noreply.github.com>
2020-03-24 17:41:08 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-echo Updating and Upgrading Linux Packages
+echo "Updating and Upgrading Linux Packages"
 
 apt-get -y update && 
 apt-get -y upgrade &&
 apt-get -y install software-properties-common &&
 
-echo Adding Ansible Repo and installing Ansible
+echo "Adding Ansible Repo and installing Ansible"
 
 apt-add-repository -y ppa:ansible/ansible &&
 apt-get -y update &&
 apt-get -y install ansible &&
 
-echo Pulling Ansible Playbook for Setup
+echo "Pulling Ansible Playbook for Setup"
 
 git clone https://github.com/alex-marrero/MDX-Ubuntu-Setup.git
 
